4b4c1aef01b2d866f164be97b76d43f091b776f2
Japan Dave <davejapan2001@gmail.com>
2023-09-11 23:46:29 +0530

    Final

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,6 @@ router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", currentUser);
+router.get("/current", validateToken, currentUser);
 
 module.exports = router;
